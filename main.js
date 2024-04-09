14cf65166fd6e1a70adf9bde7ec354663087bd47
Oricha Elizabeth <166532325+or-icha@users.noreply.github.com>
2024-04-09 17:06:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// let rice = 700;
+// let semo = 50;
+
+// // console.log(rice)
+// // console.log(semo)
+// let garri =80;
+
+// // console.log(garri)
+// let soap = 90;
+// // let accumulate rice,semo, garri and soap
+// let addNumber = rice+semo+garri+soap;
+
+// // console.log(garri)
+// // this code prints anything you give it.
+// console.log(addNumber);
+
+let bossname = "fabian";
+let tutorname = "emmanuel";
+let studyname = "agnes";
+let myname = "elizabeth";
+
+console.log(bossname);
+console.log(tutorname);
+console.log(studyname);
+console.log(myname);
+
+let sistername = "dorcas";
+let brothername = "john";
+let unclename = "george";
+
+console.log(sistername);
+console.log(brothername);
+console.log(unclename);
